1041dcbc994d6ca953430f251e7e4f9af07bf7a2
Aman Dhanjal <amandeepsdhanjal@gmail.com>
2014-12-31 18:08:41 -0500

    Initial Checkin of BuildSmoker
    A project to automate running of software's with dummy inputs and email
    there logs to the admin. The code can be run after the build is
    finished. Various configuration options available.

--- src/com/dhanjal/build/IRunSmoker.java
+++
@@ -1,7 +0,0 @@
-package com.dhanjal.build;
-
-public interface IRunSmoker {
-
-	
-	
-}
--- src/com/dhanjal/build/Report/INotify.java
+++
@@ -1,6 +0,0 @@
-package com.dhanjal.build.Report;
-
-public interface INotify {
-
-	public boolean notifyNow();
-}
